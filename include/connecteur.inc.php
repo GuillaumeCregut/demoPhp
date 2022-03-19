857f3c27eed375fddef0eac0fd71deeb86763e5b
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-19 11:03:24 +0100

    first commit

---
+++ include/connecteur.inc.php
@@ -0,0 +1,17 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 		 															 *
+ * Nom de la page :connecteur.inc.php								 *
+ * Date création :	19/03/2022										 *
+ * Date Modification : 19/03/2022									 *
+ * Créateur : Guillaume Crégut										 *													
+ * Version :	0.1 												 *
+ * Objet et notes :													 *
+ *	 Contient les informations de connexion à la base de données	 *
+ *	                              									 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+$DataBaseName = "music";
+$DataBaseUser = 'movie_user';
+$DataBasePass = 'movie_user';
+$DataBaseServeur='localhost';
+?>
